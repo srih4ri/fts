4d33e4cec08d1070ada0cc7288df90fee985ce1c
Srihari K <srihari@bangthetable.com>
2013-03-12 00:34:23 +0530

    Persist shit-list

--- fts.js
+++ fts.js
@@ -1,16 +1,33 @@
 jQuery(document).ready(function($) {
     var addImageLinks = function(){
         $('div.photoRedesign').each(function(i,e){
+            var image_link = $(e).children('a.uiPhotoThumb').attr('href');
+            chrome.storage.sync.get(image_link,function(stored_obj){
+                if(stored_obj[image_link] == 'blocked'){
+                    $(e).hide();
+                }
+            });
             $('<a>', {
                 text: 'FtS!',
                 title: 'Fuck this shit!',
                 href: '#',
                 class: 'fts-button'}
-             ).prependTo(e)
+             ).prependTo(e);
         });
+
+
     }
-    $('.fts-button').on('click',function(e){
-        $(this).parent().hide()
-    });
     addImageLinks();
+
+    $(document).on('click','.fts-button',function(e){
+        $(this).parent().hide();
+        var shitty_link = $(this).siblings('a.uiPhotoThumb').attr('href');
+        var storage_obj = {}
+        storage_obj[shitty_link] = 'blocked'
+        if(shitty_link != ''){
+            chrome.storage.sync.set(storage_obj, function() {
+                //Do Nothing
+            });
+        }
+    })
 });
